5c292cc8b1e3c1b8751d0d6628d7786ab9fcf65a
sriram164211 <kasyapchitturu@gmail.com>
2026-06-28 09:09:29 -0700

    Add toggle user

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,2 +1,30 @@
 // alert("Hello from JS")
 console.log("Hello from JS")
+var users=[
+    {
+        "Name": "John Doe",
+        "Gender": "Male",
+        "Image": "/img/john.png"
+    },
+    {
+        "Name": "Jane Doe",
+        "Gender": " Female",
+        "Image": "/img/jane.png"
+    }
+];
+var currentUserId = 1;
+var toggelButton = document.getElementById("btn-toggle");
+toggelButton.addEventListener("click", function () {
+    currentUserId = 1-currentUserId;
+    displayUser(users[currentUserId]);
+});
+
+var img = document.getElementById("user-image");
+var gender = document.getElementById("user-gender");
+var name = document.getElementById("user-name");
+
+function displayUser(userJson) {
+    name.innerText = userJson.Name;
+    gender.innerText = userJson.Gender;
+    img.src = userJson.Image;
+}
